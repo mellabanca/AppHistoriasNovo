43fa31df541bb970a440710e1834c9ddceeb2891
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-12-04 16:09:56 -0300

    Aula 82 Tela Feed e StoryCard

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,36 +1,58 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+
 import Feed from "../screens/Feed";
 import CreateStory from "../screens/CreateStory";
-
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const BottomTabNavigator = () => {
     return (
         <Tab.Navigator
+            labeled={false}
+            barStyle={styles.bottomTabStyle}
             screenOptions={({ route }) => ({
                 tabBarIcon: ({ focused, color, size }) => {
                     let iconName;
-                    if (route.name === 'Feed') {
-                        iconName = focused
-                            ? 'book'
-                            : 'book-outline';
-                    } else if (route.name === 'CreateStory') {
-                        iconName = focused ? 'create' : 'create-outline';
+                    if (route.name === "Feed") {
+                        iconName = focused ? "home" : "home-outline";
+                    } else if (route.name === "Criar História") {
+                        iconName = focused ? "add-circle" : "add-circle-outline";
                     }
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                },
+                    return (
+                        <Ionicons
+                            name={iconName}
+                            size={RFValue(25)}
+                            color={color}
+                            style={styles.icons}
+                        />
+                    );
+                }
             })}
-            tabBarOptions={{
-                activeTintColor: 'tomato',
-                inactiveTintColor: 'gray',
-            }}
+            activeColor={"#ee8249"}
+            inactiveColor={"gray"}
         >
-            <Tab.Screen name="Feed" component={Feed} options={{headerShown:false}}/>
-            <Tab.Screen name="CreateStory" component={CreateStory} options={{headerShown:false}}/>
+            <Tab.Screen name="Feed" component={Feed} />
+            <Tab.Screen name="Criar História" component={CreateStory} />
         </Tab.Navigator>
     );
-}
+};
 
-export default BottomTabNavigator
+const styles = StyleSheet.create({
+    bottomTabStyle: {
+        backgroundColor: "#2f345d",
+        height: "8%",
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        overflow: "hidden",
+        position: "absolute"
+    },
+    icons: {
+        width: RFValue(30),
+        height: RFValue(30)
+    }
+});
+
+export default BottomTabNavigator;
