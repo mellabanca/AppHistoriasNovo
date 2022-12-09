f3ea246d91789126f7ba391efa387cb8e26dd56e
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-12-09 17:33:26 -0300

    Aula 85 Firebase Login Register

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDilOs9DzpA5BSMhqyuRaDXcHBAMFgZAV4",
+    authDomain: "appnarracaohistoriasnew.firebaseapp.com",
+    projectId: "appnarracaohistoriasnew",
+    storageBucket: "appnarracaohistoriasnew.appspot.com",
+    messagingSenderId: "736019409769",
+    appId: "1:736019409769:web:6d76dfb6bcff8e7fadabf8"
+  };
---
+++ screens/Register.js
@@ -0,0 +1,202 @@
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  TextInput,
+  Alert,
+  TouchableOpacity,
+  Text
+} from "react-native";
+
+import { RFValue } from "react-native-responsive-fontsize";
+import * as Font from "expo-font";
+
+import firebase from "firebase";
+import * as SplashScreen from 'expo-splash-screen';
+SplashScreen.preventAutoHideAsync();
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf"),
+};
+
+const appIcon = require("../assets/logo.png");
+
+export default class RegisterScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      first_name: "",
+      last_name: "",
+      fontsLoaded: false,
+    };
+  }
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  registerUser = (email, password, confirmPassword, first_name, last_name) => {
+    if(password === confirmPassword){
+        firebase   
+                .auth()
+                .createUserWithEmailAndPassword(email, password)
+                .then((userCredential)=>{
+                    Alert.alert("Usuário registrado!");
+                    console.log(userCredential.user.uid)
+                    this.props.navigation.replace("Login");
+                    firebase.database().ref("/users" + userCredential.user.uid)
+                            .set({
+                                email: userCredential.user.email,
+                                first_name: first_name,
+                                last_name: last_name,
+                                current_theme: "dark"
+                            })
+                })
+                .catch(error=>{
+                    Alert.alert(error.message)
+                })   
+    } else {
+        Alert.alert("As senhas não são iguais");
+    }
+  }
+
+  render() {
+    if (this.state.fontsLoaded) {
+      SplashScreen.hideAsync();
+      const { email, password, confirmPassword, first_name, last_name} = this.state;
+
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+
+          <Text style={styles.appTitleText}>Registrar</Text>
+
+          {/* Adicione código para criar mais duas entradas de texto para nome e sobrenome */}
+          <TextInput
+            style={styles.textinput}
+            onChangeText={text => this.setState({ first_name: text })}
+            placeholder={"Nome"}
+            placeholderTextColor={"#FFFFFF"}
+
+          />
+
+          <TextInput
+            style={styles.textinput}
+            onChangeText={text => this.setState({ last_name: text })}
+            placeholder={"Sobrenome"}
+            placeholderTextColor={"#FFFFFF"}
+
+          />
+
+          <TextInput
+            style={styles.textinput}
+            onChangeText={text => this.setState({ email: text })}
+            placeholder={"Digite o e-mail"}
+            placeholderTextColor={"#FFFFFF"}
+
+          />
+          <TextInput
+            style={styles.textinput}
+            onChangeText={text => this.setState({ password: text })}
+            placeholder={"Digite a senha"}
+            placeholderTextColor={"#FFFFFF"}
+            secureTextEntry
+          />
+
+          {/* Adicione o código para criar mais uma entrada de texto para confirmar a senha */}
+          <TextInput
+            style={styles.textinput}
+            onChangeText={text => this.setState({ confirmPassword: text })}
+            placeholder={"Digite a senha novamente"}
+            placeholderTextColor={"#FFFFFF"}
+            secureTextEntry
+          />
+
+          <TouchableOpacity
+            style={[styles.button, { marginTop: 20 }]}
+            onPress={()=> this.registerUser(email, password, confirmPassword, first_name, last_name)}
+          >
+            <Text style={styles.buttonText}>Registrar</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={()=> this.props.navigation.replace("Login")}
+          >
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appIcon: {
+    width: RFValue(200),
+    height: RFValue(200),
+    resizeMode: "contain",
+    marginBottom: RFValue(20)
+  },
+  appTitleText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans",
+    marginBottom: RFValue(20)
+  },
+  textinput: {
+    width: RFValue(250),
+    height: RFValue(40),
+    padding: RFValue(10),
+    marginTop: RFValue(10),
+    borderColor: "#FFFFFF",
+    borderWidth: RFValue(4),
+    borderRadius: RFValue(10),
+    fontSize: RFValue(15),
+    color: "#FFFFFF",
+    backgroundColor: "#15193c",
+    fontFamily: "Bubblegum-Sans"
+  },
+  button: {
+    width: RFValue(250),
+    height: RFValue(50),
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    borderRadius: RFValue(30),
+    backgroundColor: "white",
+    marginBottom: RFValue(20)
+  },
+  buttonText: {
+    fontSize: RFValue(24),
+    color: "#15193c",
+    fontFamily: "Bubblegum-Sans"
+  },
+  buttonTextNewUser: {
+    fontSize: RFValue(12),
+    color: "#FFFFFF",
+    fontFamily: "Bubblegum-Sans",
+    textDecorationLine: 'underline'
+  }
+});
